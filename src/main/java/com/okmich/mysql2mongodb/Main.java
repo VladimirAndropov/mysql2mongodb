eb8f0a478552bf741f15c14d066bc22e5086172f
vladimirandropov <vvvvvvvvvv@inbox.ru>
2025-03-18 14:04:48 +0300

    addItem

--- src/main/java/com/okmich/mysql2mongodb/Main.java
+++ src/main/java/com/okmich/mysql2mongodb/Main.java
@@ -10,6 +10,7 @@ import com.okmich.mysql2mongodb.ui.AppFrame;
 import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 /**
  * @author michael.enudi
@@ -20,7 +21,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         String userInput = (String) JOptionPane.showInputDialog(
                 null,
                 "Введите данные через запятую:",
@@ -28,7 +28,7 @@ public class Main {
                 JOptionPane.QUESTION_MESSAGE,
                 null,
                 null,
-                "users,jdbc:mysql://localhost:3306/movielens,debian-sys-maint,Fpqjfwnl9KQKzrPM,mongodb://localhost:27017/movielens,movielens");
+                "jdbc:mysql://localhost:3306/movielens,debian-sys-maint,Fpqjfwnl9KQKzrPM,mongodb://localhost:27017/movielens,movielens");
 
         String[] parts = userInput.split(",");
         /**
@@ -41,51 +41,17 @@ public class Main {
          */
 
         Map<String, String> param = new HashMap<>();
-        param.put("dbServerUrl", null);
-        param.put("dbUser", null);
-        param.put("dbPassword", null);
-        param.put("mongoDbUrl", null);
-        param.put("mongoDbName", null);
+        param.put("dbServerUrl", parts[0]);
+        param.put("dbUser", parts[1]);
+        param.put("dbPassword", parts[2]);
+        param.put("mongoDbUrl", parts[3]);
+        param.put("mongoDbName", parts[4]);
 
-        String entity = param.getOrDefault( parts[0], "users");
-        String dbServerUrl = param.getOrDefault( parts[1], "jdbc:mysql://localhost:3306/movielens");
-        String dbUser =  param.getOrDefault( parts[2], "debian-sys-maint");
-        String dbPassword =  param.getOrDefault( parts[3], "Fpqjfwnl9KQKzrPM");
-        String mongoDbUrl =  param.getOrDefault( parts[4], "mongodb://localhost:27017/movielens");
-        String mongoDbName =  param.getOrDefault( parts[5], "movielens");
-
-//        if (entity == null || entity.isEmpty()) {
-//            System.err.println("Specify at least the table you wish to migrate ");
-//            System.err.println("Usage: Main -tblname=[users|movies|tags|ratings|ratings2] -mysqluser=[username] "
-//                    + "-mysqlpassword=[password] -mysqlurl=[jdbc-url] -mongourl=[mongourl] -mongodb=[mongodb]");
-//            System.exit(-1);
-//        }
-
-//        BaseMigration migrationUtil;
-//        switch (entity.toLowerCase()) {
-//            case "users":
-//                migrationUtil = new UsersMigration(jdbcUrl, jdbcUsername, jdbcPassword, mongoUrl, mongoDBName);
-//                break;
-//            case "movies":
-//                migrationUtil = new MoviesMigration(jdbcUrl, jdbcUsername, jdbcPassword, mongoUrl, mongoDBName);
-//                break;
-//            case "tags":
-//                migrationUtil = new TagsMigration(jdbcUrl, jdbcUsername, jdbcPassword, mongoUrl, mongoDBName);
-//                break;
-//            case "ratings":
-//                migrationUtil = new RatingsMigration(jdbcUrl, jdbcUsername, jdbcPassword, mongoUrl, mongoDBName);
-//                break;
-//            case "ratings2":
-//                migrationUtil = new RatingsV2Migration(jdbcUrl, jdbcUsername, jdbcPassword, mongoUrl, mongoDBName);
-//                break;
-//            default:
-//                throw new IllegalArgumentException("Unknown entity");
-//        }
-//
-//        migrationUtil.migrate();
-//
-//        System.out.println(">>>>>>>>>>>>> Done");
-//
+        String dbServerUrl = param.getOrDefault( "dbServerUrl", "jdbc:mysql://localhost:3306/movielens");
+        String dbUser =  param.getOrDefault( "dbUser", "root");
+        String dbPassword =  param.getOrDefault( "dbPassword", "password");
+        String mongoDbUrl =  param.getOrDefault( "mongoDbUrl", "mongodb://localhost:27017/movielens");
+        String mongoDbName =  param.getOrDefault( "mongoDbName", "movielens");
 
         SwingUtilities.invokeLater(() -> {
             // Создаем экземпляр формы
